a293d65d8684f056286df59dc673f541c6290c6d
leehyeonseop <gustjq4146@naver.com>
2023-04-18 17:15:34 +0900

    Section:4 종료

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import Expenses from "./components/Expenses";
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
 
 function App() {
     const expenses = [
@@ -28,9 +30,14 @@ function App() {
         },
     ];
 
+    const addExpenseHandler = (expense) => {
+        console.log('In App.js');
+        console.log(expense)
+    }
+
     return (
         <div>
-            <h2>Let's get started!</h2>
+            <NewExpense onAddExpense={addExpenseHandler} />
             <Expenses items={expenses} />
         </div>
     );
